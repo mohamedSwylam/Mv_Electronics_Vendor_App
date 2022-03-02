b8c371c07e0c00460d4e8ae0f208c39bcd67e327
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2022-03-03 01:18:20 +0200

    login

--- lib/modules/Login/login_screen.dart
+++ lib/modules/Login/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterfire_ui/auth.dart';
 import 'cubit/cubit.dart';
 import 'cubit/states.dart';
 
@@ -11,11 +13,19 @@ class LoginScreen extends StatelessWidget {
     return BlocConsumer<LoginCubit, LoginStates>(
         listener: (context, state) {},
         builder: (context, state) {
-          return  Scaffold(
-            appBar: AppBar(),
-            body: Center(
-              child: Text("Login"),
-            ),
+          return StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            // If the user is already signed-in, use it as initial data
+            initialData: FirebaseAuth.instance.currentUser,
+            builder: (context, snapshot) {
+              // User is not signed in
+              if (!snapshot.hasData) {
+                return SignInScreen(
+                    providerConfigs: []
+                );
+              }
+              return YourApplication();
+            },
           );
         }
     );
