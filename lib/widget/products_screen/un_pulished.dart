95a8f0f5bbb5703c4d1c5fdb03720525d2b817e4
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2022-03-19 22:13:02 +0200

    discount

--- lib/widget/products_screen/un_pulished.dart
+++ lib/widget/products_screen/un_pulished.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterfire_ui/firestore.dart';
@@ -7,7 +8,6 @@ import '../../../modules/add_products_screen/cubit/states.dart';
 import '../../../services/firebase_service.dart';
 import '../../modules/products_screen/cubit/cubit.dart';
 import '../../modules/products_screen/cubit/states.dart';
-
 
 class UnPublishedTab extends StatelessWidget {
   @override
@@ -28,14 +28,60 @@ class UnPublishedTab extends StatelessWidget {
                 return Text('Something went wrong! ${snapshot.error}');
               }
 
-              return ListView.builder (
-                  itemCount: snapshot.docs.length,
-                  itemBuilder: (context, index){
-                Product product = snapshot.docs[index].data();
-                return ListTile(
-                    title: Text(product.productName!),
-                );
-              });
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                    itemCount: snapshot.docs.length,
+                    itemBuilder: (context, index) {
+                      Product product = snapshot.docs[index].data();
+                      var discount =(product.regularPrice!-product.salesPrice!)-product.regularPrice!*100;
+                      return Card(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 80,
+                              width: 80,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: CachedNetworkImage(
+                                  imageUrl: product.imageUrls![0],
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(product.productName!),
+                                  if (product.salesPrice != null)
+                                    Row(children: [
+                                      Text(product.salesPrice.toString()),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        product.regularPrice.toString(),
+                                        style: TextStyle(
+                                          decoration: product.salesPrice != null
+                                              ? TextDecoration.lineThrough
+                                              : null,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ]),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+              );
             },
           );
         });
