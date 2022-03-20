d556347379720055aecc0cc7e2396d3a867b4892
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2022-03-20 02:54:49 +0200

    slidable

--- lib/widget/products_screen/un_pulished.dart
+++ lib/widget/products_screen/un_pulished.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutterfire_ui/firestore.dart';
 import 'package:mv_vendor_app/models/product_model.dart';
 import '../../../modules/add_products_screen/cubit/cubit.dart';
@@ -35,52 +36,73 @@ class UnPublishedTab extends StatelessWidget {
                     itemBuilder: (context, index) {
                       Product product = snapshot.docs[index].data();
                       var discount =(product.regularPrice!-product.salesPrice!)-product.regularPrice!*100;
-                      return Card(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 80,
-                              width: 80,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: CachedNetworkImage(
-                                  imageUrl: product.imageUrls![0],
+                      return Slidable(
+                        child: Card(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 80,
+                                width: 80,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: CachedNetworkImage(
+                                    imageUrl: product.imageUrls![0],
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              width: 8,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(product.productName!),
-                                    Row(children: [
-                                      if (product.salesPrice != null)
-                                        Text(cubit.formattedNumber(product.salesPrice)),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Text(
-                                        cubit.formattedNumber(product.regularPrice),
-                                        style: TextStyle(
-                                          decoration: product.salesPrice != null
-                                              ? TextDecoration.lineThrough
-                                              : null,
-                                          color: Colors.red,
-                                        ),
-                                      ),
-                                      SizedBox(width: 10,),
-                                      Text('${discount.toInt()}%', style: TextStyle(color: Colors.red),),
-                                    ]),
-                                ],
+                              SizedBox(
+                                width: 8,
                               ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(product.productName!),
+                                      Row(children: [
+                                        if (product.salesPrice != null)
+                                          Text(cubit.formattedNumber(product.salesPrice)),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          cubit.formattedNumber(product.regularPrice),
+                                          style: TextStyle(
+                                            decoration: product.salesPrice != null
+                                                ? TextDecoration.lineThrough
+                                                : null,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                        SizedBox(width: 10,),
+                                        Text('${discount.toInt()}%', style: TextStyle(color: Colors.red),),
+                                      ]),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        endActionPane: ActionPane(
+                        motion: ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              onPressed: (context){},
+                              backgroundColor: Color(0xFFFE4A49),
+                              foregroundColor: Colors.white,
+                              icon: Icons.delete,
+                              label: 'Delete',
+                            ),
+                            SlidableAction(
+                              onPressed: (context){},
+                              backgroundColor: Color(0xFF21B7CA),
+                              foregroundColor: Colors.white,
+                              icon: Icons.share,
+                              label: 'Share',
                             ),
                           ],
-                        ),
+                      ),
                       );
                     }),
               );
