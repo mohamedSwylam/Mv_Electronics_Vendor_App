51144f3813d705acabd5b8789a56b2abedd38481
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2022-03-27 23:16:01 +0200

    update

---
+++ lib/models/vendor_model.dart
@@ -0,0 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Vendor {
+  final bool? approved;
+  final String? businessName;
+  final String? city;
+  final String? state;
+  final String? country;
+  final String? email;
+  final String? landMark;
+  final String? logo;
+  final String? shopImage;
+  final String? mobile;
+  final String? pinCode;
+  final String? taxRegistered;
+  final Timestamp? time;
+  final String? tinNumber;
+  final String? uid;
+
+  Vendor(
+      {this.approved,
+      this.businessName,
+      this.city,
+      this.state,
+      this.country,
+      this.email,
+      this.landMark,
+      this.logo,
+      this.shopImage,
+      this.mobile,
+      this.pinCode,
+      this.taxRegistered,
+      this.time,
+      this.tinNumber,
+      this.uid});
+  Vendor.fromJson(Map<String, Object?> json)
+      : this(
+    approved:json['approved']! as bool,
+    businessName:json ['businessName']! as String,
+    city: json['city']! as String,
+    state: json['state']! as String,
+    country: json['country']! as String,
+    email: json['email']! as String,
+    landMark: json['LandMark']! as String,
+    logo: json['logo']! as String,
+    shopImage: json['shopImage']! as String,
+    mobile: json ['mobile'] as String,
+    pinCode: json['pinCode']! as String,
+    taxRegistered: json['taxRegistered']! as String,
+    time: json['time']! as Timestamp,
+    tinNumber: json['tinNumber']! as String,
+    uid: json['uid']! as String,
+  );
+  Map<String, Object?> toJson() {
+    return {
+    'approved':approved,
+    'businessName':businessName,
+    'city' :city,
+    'state':state,
+    'country':country,
+    'email':email,
+    'LandMark':landMark,
+    'logo':logo,
+    'shopImage':shopImage,
+      'mobile' :mobile,
+    'pinCode':pinCode,
+    'taxRegistered':taxRegistered,
+    'time':time,
+    'tinNumber':tinNumber,
+    'uid':uid
+    };
+  }
+}
